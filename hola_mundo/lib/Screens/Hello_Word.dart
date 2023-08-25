2ca8d756ce07961c1791f6973982deb3cfe4b811
JuanB12 <jcamilobb03@gmail.com>
2023-08-25 11:27:30 -0500

    organizaión en nav

--- hola_mundo/lib/Screens/Hello_Word.dart
+++ hola_mundo/lib/Screens/Hello_Word.dart
@@ -10,40 +10,31 @@ class Hello_word extends StatelessWidget {
       color: Colors.blue,
     );
 
+    const row = Row(
+                
+                children: [
+                  Text(
+                    'Nombre del Usuario',
+                    style: textStyle,
+                  ),
+                  Expanded(child: SizedBox()),
+                  CircleAvatar(
+                    backgroundColor: Colors.brown,
+                    child: Text('W'),
+                  )
+                ],
+              );
     return const Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             Padding(
               padding: EdgeInsets.all(8.0),
-              child: CustomerNav(textStyle),
-            ),
-            //carrusell Martin
-
-
-            //pageview josuad
-
-
+              child: row,
+            )
           ],
         ),
       ),
     );
-  }
-
-  Row CustomerNav(TextStyle textStyle) {
-    return Row(
-              
-              children: [
-                Text(
-                  'Nombre del Usuario',
-                  style: textStyle,
-                ),
-                Expanded(child: SizedBox()),
-                CircleAvatar(
-                  backgroundColor: Colors.brown,
-                  child: Text('W'),
-                )
-              ],
-            );
   }
 }
