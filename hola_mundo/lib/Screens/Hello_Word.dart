ef1fe50c0d744097d0671bf5ec203e15ea960bec
JuanB12 <jcamilobb03@gmail.com>
2023-08-25 11:56:11 -0500

    cambio

--- hola_mundo/lib/Screens/Hello_Word.dart
+++ hola_mundo/lib/Screens/Hello_Word.dart
@@ -10,19 +10,19 @@ class Hello_word extends StatelessWidget {
       color: Colors.blue,
     );
 
-    const CustomerNav = Row(                
-                children: [
-                  Text(
-                    'Nombre del Usuario',
-                    style: textStyle,
-                  ),
-                  Expanded(child: SizedBox()),
-                  CircleAvatar(
-                    backgroundColor: Colors.brown,
-                    child: Text('W'),
-                  )
-                ],
-              );
+    const CustomerNav = Row(
+      children: [
+        Text(
+          'Nombre del Usuario',
+          style: textStyle,
+        ),
+        Expanded(child: SizedBox()),
+        CircleAvatar(
+          backgroundColor: Colors.brown,
+          child: Text('W'),
+        )
+      ],
+    );
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -31,7 +31,24 @@ class Hello_word extends StatelessWidget {
               padding: EdgeInsets.all(8.0),
               child: CustomerNav,
             ),
-            
+
+            //Pafeview
+            Container(
+              height: 150.0,
+              color: Colors.red,
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            //listview
+            Container(
+              height: 150.0,
+              color: Colors.green,
+              child: ListView(children: [
+                Container(height: 50.0, child: Text('1'), color: Colors.amber,),
+                Text('2'),
+              ]),
+            )
           ],
         ),
       ),
