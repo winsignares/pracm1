c98b56fcf10a8f6624e98b4cbf578f529f951392
JuanB12 <jcamilobb03@gmail.com>
2023-08-25 11:07:00 -0500

    customerNave add en home

--- hola_mundo/lib/Screens/Hello_Word.dart
+++ hola_mundo/lib/Screens/Hello_Word.dart
@@ -6,9 +6,16 @@ class Hello_word extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
+      body: SafeArea(
+        child:  Column(
+        children: [
+          Row(
+            children: [
+              Text('Nombre del Usuario', style: TextStyle( fontSize: 24.0, color: Colors.blue,),)
+            ],
+          )
+        ],
+      ),),
     );
   }
 }
