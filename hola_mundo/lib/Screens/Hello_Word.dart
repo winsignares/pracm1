469e49d4f4479958e8b4b4da748fadbf1a3d2ecd
JuanB12 <jcamilobb03@gmail.com>
2023-08-25 11:14:44 -0500

    organización de style en text

--- hola_mundo/lib/Screens/Hello_Word.dart
+++ hola_mundo/lib/Screens/Hello_Word.dart
@@ -5,17 +5,25 @@ class Hello_word extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      fontSize: 24.0,
+      color: Colors.blue,
+    );
     return const Scaffold(
       body: SafeArea(
-        child:  Column(
-        children: [
-          Row(
-            children: [
-              Text('Nombre del Usuario', style: TextStyle( fontSize: 24.0, color: Colors.blue,),)
-            ],
-          )
-        ],
-      ),),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Nombre del Usuario',
+                  style: textStyle,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
