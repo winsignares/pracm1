f181d4c8c9838c2410726a3386623824cbd1a862
JuanB12 <jcamilobb03@gmail.com>
2023-08-25 10:32:30 -0500

    agg archivo

---
+++ hola_mundo/lib/Screens/Hello_Word.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Hello_word extends StatelessWidget {
+  const Hello_word({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
---
+++ hola_mundo/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:hola_mundo/Screens/MyApp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
