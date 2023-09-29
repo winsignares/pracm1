fe8a2ec79b0c5cc9f5add4dace060e45875dad7a
JuanB12 <jcamilobb03@gmail.com>
2023-09-29 11:31:18 -0500

    se termino el sharepreferences

--- hola_mundo/lib/Screens/MyHombePage.dart
+++ hola_mundo/lib/Screens/MyHombePage.dart
@@ -1,33 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:hola_mundo/localstorage/Sharepreference.dart';
 
 import '../Widgets/CustomerDrawer.dart';
 
 class MyHomePage2 extends StatelessWidget {
-  const MyHomePage2({super.key});
+  MyHomePage2({super.key});
   static const String nombre = 'Home';
+  final prefs = PrefernciaUsuario();
   @override
   Widget build(BuildContext context) {
+    prefs.ultimapagina = MyHomePage2.nombre;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Principal",
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue,
+        backgroundColor: prefs.colosecundario ? Colors.blue : Colors.red,
       ),
       drawer: const CustmerDrawer(),
       body: SafeArea(
         child: Center(
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: const Column(
+            child:  Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Color: '),
-                Divider(),
-                Text('Sexo: '),
-                Divider(),
-                Text('Nombre : '),
+                Text('Color: ${prefs.colosecundario}'),
+               const Divider(),
+                Text('Sexo: ${prefs.sexo}'),
+                const Divider(),
+                Text('Nombre : ${prefs.nombre}'),
               ],
             ),
           ),
