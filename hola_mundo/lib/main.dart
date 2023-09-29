41f7f80209283cf493b5d6d01e2b778246d236e1
JuanB12 <jcamilobb03@gmail.com>
2023-09-29 11:05:38 -0500

    se inicia el patron singlenton en el Main

--- hola_mundo/lib/main.dart
+++ hola_mundo/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hola_mundo/Screens/MyApp.dart';
+import 'package:hola_mundo/localstorage/Sharepreference.dart';
 
 
-void main() {
+void main() async {
+  
+  final prefs = PrefernciaUsuario();
+  await prefs.initPrefs();
+
   runApp(const MyApp());
 }
 
